1b9215dccf1f85d920f5f3fc9d0db589cb884606
Andrew-booler <251563149@qq.com>
2016-07-02 17:13:59 +0800

    test

---
+++ ActivityWeb/src/com/activityweb/action/LangAction.java
@@ -0,0 +1,17 @@
+package com.activityweb.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LangAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4917102764822692922L;
+
+	@Override
+	public String execute() throws Exception {
+		return super.execute();
+	}
+
+}
